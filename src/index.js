af864251da7f2b15a9850871d5ae781414e86a54
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-13 03:42:02 +0500

    Create a function for adding a new task

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import './style.css';
 
 const submitForm = document.querySelector('form');
 const taskInput = document.querySelector('#task-input');
+const todoList = document.querySelector('.list-group');
 
 const tasks = [];
 
 submitForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  tasks.push({description: taskInput.value, completed: false, index: tasks.length})
+  tasks.push({ description: taskInput.value, completed: false, index: tasks.length });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 
-  localStorage.setItem('tasks', JSON.stringify(tasks))
+  todoList.innerHTML = '';
+
+  displayTasksFromLS();
 
   taskInput.value = '';
+
   console.log(tasks);
 });
+
+
+const displayTasksFromLS = () => {
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+
+    li.classList = 'list-group-item';
+    li.setAttribute('draggable', 'true');
+    li.innerHTML = `
+    <input type="checkbox" class="checkbox" checked="${task.completed}">
+    <label>${task.description}</label>
+    <i class="fa fa-ellipsis-v drag-drop float-end"></i>
+    `;
+
+    document.querySelector('.list-group').appendChild(li);
+  });
+};
